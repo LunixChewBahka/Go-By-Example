faaa4a78928ef62d3790bbdad146907516d2e388
lunixchewbahka <lunixchewbahka@gmail.com>
2017-05-23 18:13:19 +0800

    Getting to know functions in Golang

--- 98_Functions/functions.go
+++ 98_Functions/functions.go
@@ -8,6 +8,10 @@ func main() {
 
 	doSomething()
 	add(2, 4, 10)
+
+	// result then receives the returned value from the func
+	result := superComplexCalculation()
+	fmt.Println(result)
 }
 
 func doSomething() {
@@ -21,4 +25,12 @@ func function_name(var <type> ....params) <return type> {
 */
 func add(a int, b int, c int) {
 	fmt.Println(a + b*c)
+}
+
+// function return
+func superComplexCalculation() int {
+	// this function is executed then...
+	e := 65*2/(2+5) - 6*2
+	// e is returned as an int
+	return e
 }
