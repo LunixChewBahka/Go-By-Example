16e17f7664b2aac2bdec0f41712e3eb31517486e
lunixchewbahka <lunixchewbahka@gmail.com>
2017-06-04 21:45:03 +0800

    Refresh pointers

---
+++ 15_Pointers/04_pointer/stacks_and_pointers.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	// Declare variable of type int with a value of 10.
+	count := 10
+
+	// Display the "value of" and "address of" count.
+	fmt.Println("count:\tValue Of[", count, "]\tAddr Of [", &count, "]")
+
+	// Pass the "value of" the count.
+	increment(count)
+
+	fmt.Println("count:\tValue Of[", count, "]\tAddr Of [", &count, "]")
+}
+
+// go:noincline
+func increment(inc int) {
+	// Increment the "value of" inc.
+	inc++
+	fmt.Println("count:\tValye Of[", inc, "]\tAddr Of [", &inc, "]")
+}
